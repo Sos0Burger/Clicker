afa61e7e274c3b16b9c3500c1a425aadbc685500
SosoBurger <stulnikovan1337@gmail.com>
2022-03-07 08:41:49 +0300

    s

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -6,29 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+import android.os.Bundle;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
-    int counter = 0;
-    int scaling = 1;
-    int limit = 10;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    private Integer counter = 0;
+    private Integer limit = 10;
     public void onButtonAdd(View view){
-        counter = counter + 1*scaling;
+        counter++;
         TextView counterView = (TextView) findViewById(R.id.textView);
         TextView FigureOval = (TextView) findViewById(R.id.textView2);
         if (counter >= limit){
             ViewGroup.LayoutParams ovalparams = counterView.getLayoutParams();
-            ovalparams.width = (FigureOval.getWidth() + 25);
-            ovalparams.height = (FigureOval.getHeight() + 25);
+            ovalparams.width = (FigureOval.getWidth() + 50);
+            ovalparams.height = (FigureOval.getHeight() + 50);
             FigureOval.setLayoutParams(ovalparams);
             limit *= 10;
         }
         counterView.setText(Integer.toString(counter));
     }
+
 
 }
